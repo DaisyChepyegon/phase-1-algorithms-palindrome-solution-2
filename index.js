e3c7aeaee553a5c278acebae1fb889576706b857
DaisyChepyegon <daisychepyegon@gmail.com>
2022-06-16 12:57:26 +0300

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for(let i=0; i<word.length/2; i++){
+    const j=word.length-1-i;
+
+    if(word[i] !==word[j]){
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
@@ -9,6 +17,35 @@ function isPalindrome(word) {
 /*
   Add written explanation of your solution here
 */
+if (require.main === module) {
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+}
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
